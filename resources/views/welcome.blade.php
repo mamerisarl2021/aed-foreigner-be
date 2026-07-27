83e9f875c5c9ad53ff5fdf08935f61e8bda46f3a
ctrldavieddee <princedavealex.20@gmail.com>
2026-07-27 16:26:16 +0100

    update: use config wrapper on root redirect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; url=https://local-aed.qcdigitalhub.com/" />
+    <meta http-equiv="refresh" content="0; url={{ config('app.frontend_url') }}" />
 </head>
 </html>
